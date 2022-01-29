35c2f5d8572193c8dec5b7087233653a91c01d86
FJ <37023430+fjlenz@users.noreply.github.com>
2022-01-29 21:58:42 +0100

    add uppercase Stream trials

--- src/main/java/com/fj/Java17Playground/Java17PlaygroundApplication.java
+++ src/main/java/com/fj/Java17Playground/Java17PlaygroundApplication.java
@@ -2,6 +2,7 @@ package com.fj.Java17Playground;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Java17PlaygroundApplication {
@@ -13,28 +14,44 @@ public class Java17PlaygroundApplication {
 
 	public static void runItNow() {
 		List<PersonEntity> names = Arrays.asList(
-				new PersonEntity(1, "Horst", "Hebert", 20),
+				new PersonEntity(1, "Horst", "Hebert", 7),
 				new PersonEntity(2, "Hans", "Robert", 18),
-				new PersonEntity(3, "Daniel", "Honk", 48)
+				new PersonEntity(3, "Daniel", "Honk", 34)
 		);
 
-		System.out.println("... foreach");
+		System.out.println("... foreach with Lambda");
 		names.forEach(personEntity -> System.out.println(personEntity));
 
 		System.out.println("... foreach with :: double colon operator");
 		names.forEach(System.out::println);
 
-		System.out.println("... for");
+		System.out.println("... for loop");
 		for (PersonEntity name : names) {
 			System.out.println(name);
 		}
 
-		System.out.println("... stream filter bigger than 2");
+		System.out.println("... stream - map firstName to upper case");
+		names.stream()
+						.map(p -> p.getFirstName().toUpperCase())
+								.forEach(p -> System.out.println("uppercase: " + p));
+
+		System.out.println("... stream - add age + 10 inside existing PersonEntity list");
+		names.stream()
+						.forEach(personEntity -> personEntity.setAge(personEntity.getAge() + 10));
+		names.forEach(System.out::println);
+
+		System.out.println("... stream - uppercase - with .map to create new List");
+		List<PersonEntity> namesUppercase = names.stream()
+				.map(p -> new PersonEntity(p.getId(), p.getFirstName().toUpperCase(), p.getLastName().toUpperCase(), p.getAge()))
+				.toList();
+		namesUppercase.forEach(System.out::println);
+
+		System.out.println("... stream filter with ID bigger than 1");
 		names.stream()
 				.filter(p -> p.getId() > 1)
 				.forEach(p -> System.out.println("after filter: " + p));
 
-		System.out.println("... Flatmap");
+		System.out.println("... flatmap");
 		String[][] arrayOfArraysOfChars = new String[][]{{"a", "b"}, {"c", "d"}, {"e", "f"}};
 
 		String[] result = Arrays.stream(arrayOfArraysOfChars).flatMap(Stream::of).toArray(String[]::new);
@@ -43,7 +60,9 @@ public class Java17PlaygroundApplication {
 		}
 
 		System.out.println("... Flatmap Filter");
-		Arrays.stream(arrayOfArraysOfChars).flatMap(Stream::of).filter(c -> !"a".equals(c)).forEach(System.out::println);
+		Arrays.stream(arrayOfArraysOfChars)
+				.flatMap(Stream::of)
+				.filter(c -> !"a".equals(c)).forEach(System.out::println);
 
 	}
 
